827e570cb48c2fb3205a81a0ea299355e0b33344
sylviaGG <jiaqi.guo@tuni.fi>
2021-02-28 19:32:16 +0200

    CNN corrected and done

--- cnn.py
+++ cnn.py
@@ -1,37 +1,35 @@
 
 from torch import Tensor, tensor
-from torch.nn import Module, Conv1d, MaxPool1d, ReLU, Linear, Dropout1d,Sequential
+from torch.nn import Module, Conv1d, MaxPool1d, ReLU, Linear, Dropout,Sequential
 
 
+## needs input dtype= float
 class CNNSystem(Module):
 
-    def __init__(self,input_size:int,
-                 dropout: float) \
+    def __init__(self) \
             -> None:
 
         super().__init__()
 
         # First layer : CNN xx neurons
 
-        self.layer1 = Sequential(Conv1d(input_size, 32, kernel_size=15),
+        self.layer1 = Sequential(Conv1d(1, 32, kernel_size=15,padding=7),
                                  ReLU(),
-                                 MaxPool1d(kernel_size=6))
+                                 MaxPool1d(kernel_size=4))
 
         # 2nd layer : CNN 32 neurons padding c6
-        self.layer2 = Sequential(Conv1d(32, 32, kernel_size=15),
+        self.layer2 = Sequential(Conv1d(32, 32, kernel_size=15,padding=7),
                                  ReLU(),
                                  MaxPool1d(kernel_size=6))
         
         #3rd layer CNN 16 neurons
-        self.layer3 = Sequential(Conv1d(16, 16, kernel_size=15),
+        self.layer3 = Sequential(Conv1d(32, 16, kernel_size=15,padding=7),
                                  ReLU(),
                                  MaxPool1d(kernel_size=5))
         # need to know input size for 1st mlp layers
-        self.mlp1 = Linear(in_features, 10)
+        self.mlp1 = Linear(16, 10)
         self.mlp2 = Linear(10,5)
 
-        # Dropout
-        self.dropout = Dropout1d(dropout)
 
     def forward(self, x: Tensor) \
             -> Tensor:
@@ -39,24 +37,17 @@ class CNNSystem(Module):
         #t = x[0]
 
         # Neural network layer stacking
-        t = self.layer1(x)
+        x = self.layer1(x)
+
         
-        t = self.layer2(t)
-        t = self.mlp1(t)
+        x = self.layer2(x)
+
+        x = x.view(x.size(0), -1)
+        t = self.mlp1(x)
 
 
-
-        # t.shape: torch.Size([4, 32, 80, 4]) -> torch.Size([4, 80, 32, 4])
-        # Tensor reshaping
-        # Permute rotates tensor dimensions preserving the data order
-        #a_t = t.permute(0, 2, 1, 3)
-
-        # View maps from one dimensionality to another sequentially
-        # Contiguous added for persisting the order during dimensionality change
-        # Parameter len is length of the tensor and -1 indicates last member
-        #h = a_t.contiguous().view(len(x), -1)
         
-        y_hat = self.Dropout1d(self.mlp2(t))
+        y_hat = self.mlp2(x)
 
         return y_hat
 
